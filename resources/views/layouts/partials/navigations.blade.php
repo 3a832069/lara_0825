82b517e66e963b093ad6948b88646573f42f55ef
Len-En Chiu <linda34264622@gmail.com>
2021-10-31 13:36:27 +0800

    設定 Controller跟路由

--- resources/views/layouts/partials/navigations.blade.php
+++ resources/views/layouts/partials/navigations.blade.php
@@ -7,18 +7,7 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('posts.index')}}">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('posts.about')}}">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('posts.show')}}">Sample Post</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="{{route('posts.contact')}}">Contact</a>
-                </li>
+
             </ul>
         </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\PostsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('posts', [PostsController::class, 'index'])->name('posts.index');
+Route::get('post', [PostsController::class, 'show'])->name('posts.show');
+Route::get('contact', [PostsController::class, 'contact'])->name('posts.contact');
+Route::get('about', [PostsController::class, 'about'])->name('posts.about');
