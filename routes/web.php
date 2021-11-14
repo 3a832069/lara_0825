726cb814d8e14e101cb1b7e742ce910ede265b4e
Len-En Chiu <linda34264622@gmail.com>
2021-11-14 19:59:07 +0800

    collection_多筆貼文的集合

--- routes/web.php
+++ routes/web.php
@@ -50,7 +50,10 @@ Route::get('/', function () {
 
     //   Post::destroy(2);
 
-       Post::destroy(3, 5, 7);
+    //    Post::destroy(3, 5, 7);
+
+       $allPosts=Post::all();
+        dd($allPosts);
 
 
 });
